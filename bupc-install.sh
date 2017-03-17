6dc79902746edc596a0c9c09279e8a22f900eb22
Kolen Cheung <christian.kolen@gmail.com>
2017-03-16 20:35:39 -0700

    bupc-install.sh: refactor helper functions download & decompress

--- bupc-install.sh
+++ bupc-install.sh
@@ -11,7 +11,7 @@ where:
 	-c	CC. Default: cc
 	-p	CXX. Default: c++"
 
-# getopts ######################################################################
+# Helper functions #############################################################
 
 gnumake() {
 	if hash gmake 2>/dev/null; then
@@ -30,6 +30,31 @@ mkdirerr() {
 		exit 1
 	fi
 }
+
+download() {
+	filename="${@##*/}"
+	if [[ ! -f "$filename" ]]; then
+		curl "$@" -O .
+		if [[ ! -f "$filename" ]]; then
+			printf "Cannot download %s from %s\n" "$filename" "$@" >&2
+			exit 1
+		fi
+	else
+		printf "%s already exist. Use the existing %s instead.\n" "$filename" "$filename"
+	fi
+}
+
+decompress() {
+	folderName="${@%.tar.gz}"
+	if [[ ! -d "$folderName" ]]; then
+		tar -xzf "$@"
+	else
+		printf "%s already existed. To remove it, run\nrm -r %s\n" "$folderName" "$(realpath "$folderName")" >&2
+		exit 1
+	fi
+}
+
+# getopts ######################################################################
 
 # reset getopts
 OPTIND=1
@@ -67,18 +92,12 @@ done
 
 # get the absolute path of outdir
 outdir=$(realpath outdir)
-
 # get the filenameBUPC from the urlBUPC
 filenameBUPC="${urlBUPC##*/}"
 folderNameBUPC="${filenameBUPC%.tar.gz}"
-
 # get the filenameBUPC from the urlTranslator
 filenameTranslator="${urlTranslator##*/}"
 folderNameTranslator="${filenameTranslator%.tar.gz}"
-
-if [[ $DEBUG ]]; then
-	printf "%s\n" "$outdir" "$urlBUPC" "$local" "$urlTranslator" "$CC" "$CXX" "$filenameBUPC" "$folderNameBUPC"
-fi
 
 # dirs within outdir
 tempdir="$outdir/temp"
@@ -90,6 +109,25 @@ translatordir="$outdir/translator"
 # warning message for translator without gmake
 if [[ ! -z "$local" ]] && [[ $(uname) == "Darwin" ]]; then
 	printf "%s\n" "Note that compiling the translator requires GNU make." "If the installation failed, install GNU make by" "brew install homebrew/dupes/make"
+fi
+
+if [[ $DEBUG ]]; then
+	printf "%s\n" \
+		"$outdir" \
+		"$urlBUPC" \
+		"$local" \
+		"$urlTranslator" \
+		"$CC" \
+		"$CXX" \
+		"$filenameBUPC" \
+		"$folderNameBUPC" \
+		"$filenameTranslator" \
+		"$folderNameTranslator" \
+		"$tempdir" \
+		"$bupcdir" \
+		"$bupcbin" \
+		"$bupcman" \
+		"$translatordir"
 fi
 
 # Download #####################################################################
@@ -113,41 +151,14 @@ fi
 cd "$tempdir"
 
 # download bupc
-if [[ ! -f "$filenameBUPC" ]]; then
-	curl "$urlBUPC" -O .
-	if [[ ! -f "$filenameBUPC" ]]; then
-		printf "Cannot download %s from %s\n" "$filenameBUPC" "$urlBUPC" >&2
-		exit 1
-	fi
-else
-	printf "%s already exist. Use the existing %s instead.\n" "$filenameBUPC" "$filenameBUPC"
-fi
-# uncompress
-if [[ ! -d "$folderNameBUPC" ]]; then
-	tar -xzf "$filenameBUPC"
-else
-	printf "%s already existed. To remove it, run\nrm -r %s\n" "$folderNameBUPC" "$(realpath "$folderNameBUPC")" >&2
-	exit 1
-fi
+download "$urlBUPC"
+# decompress
+decompress "$filenameBUPC"
 
 # download translator
 if [[ ! -z "$local" ]]; then
-	if [[ ! -f "$filenameTranslator" ]]; then
-		curl "$urlTranslator" -O .
-		if [[ ! -f "$filenameTranslator" ]]; then
-			printf "Cannot download %s from %s\n" "$filenameTranslator" "$urlTranslator" >&2
-			exit 1
-		fi
-	else
-		printf "%s already exist. Use the existing %s instead.\n" "$filenameTranslator" "$filenameTranslator"
-	fi
-	# uncompress
-	if [[ ! -d "$folderNameTranslator" ]]; then
-		tar -xzf "$filenameTranslator"
-	else
-		printf "%s already existed. To remove it, run\nrm -r %s\n" "$folderNameTranslator" "$(realpath "$folderNameTranslator")" >&2
-		exit 1
-	fi
+	download "$urlTranslator"
+	decompress "$filenameTranslator"
 fi
 
 # Compile ######################################################################
